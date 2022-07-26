b9802a40825fdef356133b40f8d4add888b108df
DarrenVargas <106000764+DarrenVargas@users.noreply.github.com>
2022-07-25 22:52:51 -0600

    Caso de Simulación - Juego de Bolos
    Se va a realizar un programa que permita calcular el puntaje de un juego de bolos.
    
    El juego consta de 10 turnos. En cada turno el jugador tiene 2 oportunidades para derribar los 10 bolos. El puntaje de cada turno se calcula con el número total de bolos derribados, sumado los puntos extras por moñonas (strikes) o medias (spares).
    
    Si el jugador derriba los 10 bolos en el primer lanzamiento del turno, hace moñona(strike), no necesita realizar el segundo lanzamiento del turno. El puntaje para este turno es 10, más el número de bolos derribados en los dos siguientes lanzamientos de bola.
    
    Si el jugador derriba los bolos en dos lanzamientos del turno hace una media (spare). El puntaje para este turno será 10, sumando los puntos extras por el número de bolos que haya derribado en el siguiente lanzamiento de bola.
    
    Si en el último turno un jugador realiza un strike o spare, se le permite lanzar las bolas extra para completar el turno. Sin embargo, no se permite el lanzamiento de más de 3 bolas en este turno.

---
+++ Juego de Bolos/src/test/java/com/example/BowlingGame/BowlingGame.java
@@ -0,0 +1,47 @@
+package com.example.BowlingGame;
+
+public class BowlingGame {
+
+    private int score = 0;
+    private int[] rolls = new int[21];
+    private int currentRolls = 0;
+
+    public void roll(int pins) {
+        score += pins;
+        rolls[currentRolls++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int frameIndex = 0;
+        for(int frame = 0; frame < 10; frame++){
+            if(isStrike(frameIndex)){
+                score += 10 + strikeBonus(frameIndex);
+                frameIndex ++;
+            }else if (isSpare(frameIndex)) {
+                score += 10 + spareBonus(frameIndex);
+                frameIndex += 2;
+            }else{
+                score += sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int frameIndex) {
+        return rolls[frameIndex] == 10;
+    }
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
+    }
+    private int strikeBonus(int frameIndex){
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+    private boolean isSpare(int frameIndex) {
+        return sumOfBallsInFrame(frameIndex) == 10;
+    }
+}
